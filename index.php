51243f9b8b436883e8758e158c3be4afa2cfedfd
yohanawi <yohanawishka2000326@gmail.com>
2022-08-20 23:30:31 +0530

    commit

--- index.php
+++ index.php
@@ -39,8 +39,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<div class="w3_agile_banner_top">
 				<div class="agile_phone_mail">
 					<ul>
-						<li><i class="fa fa-phone" aria-hidden="true"></i>+(94) 002 100 500</li>
-						<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@Companyonline.net">info@example.com</a></li>
+						<li><i class="fa fa-phone" aria-hidden="true"></i>+(94) 111 222 333</li>
+						<li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@Companyonline.net">call@example.com</a></li>
 					</ul>
 				</div>
 			</div>
@@ -53,7 +53,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<h1><a class="navbar-brand" href="index.php"><img src="images/logo.png" class="img-responsive"></a></h1>
+						<h1><a class="navbar-brand" href="index.php"><img src="images/customer-care-call-center-icon-symbol-logo-design-template-163689068.png" width="20%" class="img-responsive"></a></h1>
 					</div>
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
@@ -74,28 +74,13 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					<div class="flexslider">
 						<ul class="slides">
 							<li>
-								<h3>Lorem ipsum dolor ..?</h3>
-								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+								<h3>Customer Service !</h3>
+								<p>We offer instant and quality</p>
 							</li>
-							<li>
-								<h3>Our Mission.. </h3>
-								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-							</li>
-							<li>
-								<h3>Lorem ipsum dolor </h3>
-								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-							</li>
-							<li>
-								<h3>Our Vision... </h3>
-								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-							</li>
-							<li>
-								<h3>Lorem ipsum dolor? </h3>
-								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p> <i style="color: orange;">--find out more</i>
-							</li>
+							
 						</ul>
 					</div>
-					<a href="./client/clogin.php"><button class="btn-1">Get Start</button></a>
+					<a href="#about"><button class="btn-1">More about us</button></a>
 				</section>
 				<!-- flexSlider -->
 				<script defer src="js/jquery.flexslider.js"></script>
